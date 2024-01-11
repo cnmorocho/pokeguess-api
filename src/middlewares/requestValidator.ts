e0a838f3ff66141ca88f4ad9c0d5bd6f6bcdaccb
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2024-01-11 01:26:47 -0300

    Add query params validator

---
+++ src/middlewares/requestValidator.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, z } from 'zod';
+import { RequestValidationMiddleware } from '../types/CustomTypes';
+
+export function validateRequestQueries(
+  schema: AnyZodObject,
+): RequestValidationMiddleware {
+  return async function (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const queryParams = req.query;
+      await schema.parseAsync(queryParams);
+      next();
+    } catch (error) {
+      let err = error;
+      if (err instanceof z.ZodError) {
+        err = err.issues.map((e) => ({ path: e.path[0], message: e.message }));
+      }
+      res.status(400).json({
+        status: 'failed',
+        error: err,
+      });
+    }
+  };
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { guessPokemon } from '../controllers/pokemonController';
 import { playGame } from '../controllers/gameController';
+import { validateRequestQueries } from '../middlewares/requestValidator';
+import { queryParamsSchema } from '../schemas/playSchema';
 
 const router = Router();
 
 router.get('/guess', guessPokemon);
-router.post('/play', playGame);
+router.post('/play', validateRequestQueries(queryParamsSchema), playGame);
 
 export default router;
